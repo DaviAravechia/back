40972da5e520ae157442f391eafee9e797b2809a
DaviAravechia <alferdavi@gmail.com>
2024-11-07 21:46:50 -0300

    mudando nome de uuid nas tabelas para id, sobreescrevndo o padrão

--- API/models.py
+++ API/models.py
@@ -3,17 +3,28 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
-class Paciente(models.Model):
-    uuid = models.UUIDField(primary_key=True, editable=False)
+class Pacientes(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
     nome = models.CharField(max_length=255)
     data_nascimento = models.DateField()
     telefone = PhoneNumberField()
     email = models.EmailField()
-    historico_medico = models.TextField()  # Ajustado para TextField
+    historico_medico = models.TextField()  # Ajustado para TextField'
     cpf = models.CharField(max_length=11,)
 
 
     def __str__(self):
         return self.nome
-
+    
+class Consultas(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    paciente = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    data_e_hora_consulta = models.DateTimeField()
+    descricao = models.TextField()
+    status = models.CharField(max_length=20, choices=[
+        ('agendada', 'Agendada'),
+        ('concluida', 'Concluida'),
+        ('cancelada', 'Cancelada')
+    ]
+)
 
