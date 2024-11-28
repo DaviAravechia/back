37fdbbbd9f8b06c024abeb8bde4267f5cb1a7097
João Paulo Nicolas e Lima <pouljp@gmail.com>
2024-11-28 18:11:53 -0300

    funcinando cadastro

--- API/views.py
+++ API/views.py
@@ -84,14 +84,7 @@ def create_paciente(request):
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def create_paciente(request):
-    serializer = PacientesSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
+
 @api_view(['POST'])
 def register_user(request):
     """
